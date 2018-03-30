1f4216e522f48123df76802a715d08e03e1641fd
thanhnn96 <nnt296@gmail.com>
2018-03-30 18:21:46 +0700

    edit route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:api')->get('/user/dogs', function (Request $request) {
+    $user = $request->user();
+    return $user->dogs()->get();
+});
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,3 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('user', 'UserController');
-
-Route::get('/user/showDog/{id}', 'UserController@showDog')->name('user.showDog');
-
